264d0d2a283644aebe6847072f4df53ad6c5a694
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2021-06-05 21:21:47 +0800

    Delete api directory

--- api/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "username": "qjeldbsxdmazul",
-    "password": "19e5c4c5d801c0befddc40c5b3e78b556192dede7b8eff3642663b3e029221c3",
-    "database": "d3gf4t22v6gsn2",
-    "host": "ec2-18-215-111-67.compute-1.amazonaws.com",
-    "dialect": "postgres",
-    "logging": false
-};

fe8ecbcc454a834a6572269be72cf35130b5ccdd
Ainal-Farhan <mohd.ainalfarhan7@gmail.com>
2021-06-05 21:19:24 +0800

    reinitialized the backend setup

--- api/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "username": "postgres",
-    "password": "^%1N4lpostgresql990311",
-    "database": "managedodb",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "logging": false
-};
--- server.js
+++ server.js
@@ -5,32 +5,43 @@ const app = express();
 
 app.use(cors());
 
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./api/models/index.js");
+const db = require("./models/index.js");
 
-// // Drop existing table and re-sync database
-// db.sequelize.sync({ force: true }).then(() => {
-//         console.log("Drop and re-sync db.");
-//    });
-
-db.sequelize.sync().then(() => {
-    console.log("sync db status: done");
+app.get('/', function(req, res) {
+    db.Url.findAll({ order: [
+                ['createdAt', 'DESC']
+            ], limit: 5 })
+        .then(urlObjs => {
+            res.render('index', {
+                urlObjs: urlObjs
+            });
+        });
 });
-
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Managedo application database" });
-});
-
-// api routes
-require("./api/routes/user.routes.js")(app);
 
 app.use(express.json());
+
+app.post('/url', function(req, res) {
+    const url = req.body.url
+
+    urlShortener.short(url, function(err, shortUrl) {
+        db.Url.findOrCreate({ where: { url: url, shortUrl: shortUrl } })
+            .then(([urlObj, created]) => {
+                res.send(shortUrl)
+            });
+    });
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;

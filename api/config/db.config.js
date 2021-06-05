50bfcdb41dc673ec3af7337f26903cb47f27de66
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2021-06-05 19:42:44 +0800

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "username": "demo_user",
-    "password": "demo_user",
-    "database": "express_demo_test",
-    "host": "127.0.0.1",
+    "username": "qjeldbsxdmazul",
+    "password": "19e5c4c5d801c0befddc40c5b3e78b556192dede7b8eff3642663b3e029221c3",
+    "database": "d3gf4t22v6gsn2",
+    "host": "ec2-18-215-111-67.compute-1.amazonaws.com",
     "dialect": "postgres",
     "logging": false
 };

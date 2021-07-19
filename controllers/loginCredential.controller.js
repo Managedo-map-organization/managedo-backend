2b2992aec35231ce873423649653cb46d5b01d54
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2021-07-19 16:32:51 +0800

    Update loginCredential.controller.js

--- controllers/loginCredential.controller.js
+++ controllers/loginCredential.controller.js
@@ -8,7 +8,7 @@ exports.addOne = (req, res) => {
 
     LoginCredential.create(jsonData)
         .then(data => {
-            res.send(JSON.parse(JSON.stringify(`"auto-generated ID":${data.id}`)));
+            res.send(JSON.parse(JSON.stringify(data)));
         })
         .catch(err => {
             res.status(500).send({
